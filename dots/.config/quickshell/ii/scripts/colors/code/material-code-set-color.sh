b61414d3b8b4d8b51bf21f80886fffef36c5d054
Javier Rolando <javier.rolando98@hotmail.com>
2025-10-29 22:46:15 -0300

    uncommented default profile

--- dots/.config/quickshell/ii/scripts/colors/code/material-code-set-color.sh
+++ dots/.config/quickshell/ii/scripts/colors/code/material-code-set-color.sh
@@ -3,7 +3,7 @@ COLOR_FILE_PATH="${XDG_STATE_HOME:-$HOME/.local/state}/quickshell/user/generated
 
 # Define an array of possible VSCode settings file paths for various forks
 settings_paths=(
-    # "${XDG_CONFIG_HOME:-$HOME/.config}/Code/User/settings.json"
+    "${XDG_CONFIG_HOME:-$HOME/.config}/Code/User/settings.json"
     # "${XDG_CONFIG_HOME:-$HOME/.config}/VSCodium/User/settings.json"
     # "${XDG_CONFIG_HOME:-$HOME/.config}/Code - OSS/User/settings.json"
     # "${XDG_CONFIG_HOME:-$HOME/.config}/Code - Insiders/User/settings.json"
